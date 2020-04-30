248c78d10c98d492bad7ef2e0d264fccc6ec4f83
killofki <killofki@yahoo.co.kr>
2020-05-01 02:35:15 +0900

    end space

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
+ 
+const mfsjea = require('./mfsjea.js') 
 
-const mfsjea = require('./mfsjea.js')
+const readline = require('readline') 
 
-const readline = require('readline')
+const rl = readline.createInterface({ 
+  input: process.stdin, 
+  output: process.stdout, 
+  prompt: '> ' 
+}); 
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: '> '
-});
-
-rl.on('line', (line) => {
-  const result = mfsjea.jeamfs(line)
-  console.log('BEST RESULT: ' + result.str + ' (' + result.source + '-' + result.destination + ', score: ' + result.score + ')')
-})
+rl.on('line', (line) => { 
+  const result = mfsjea.jeamfs(line) 
+  console.log('BEST RESULT: ' + result.str + ' (' + result.source + '-' + result.destination + ', score: ' + result.score + ')') 
+}) 
